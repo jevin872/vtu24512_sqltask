8ac9c54af085006c1e28984b54e35f31f33168d1
jevin872 <vtu24512@veltech.edu.in>
2026-04-19 23:22:52 +0530

    Update vtu24512task2.sql

--- vtu24512task2.sql
+++ vtu24512task2.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-2. Create Tables
-Student Table
+-- 2. Create Tables
+-- Student Table
 CREATE TABLE Student (
     VTU_Number VARCHAR(20) PRIMARY KEY,
     Name VARCHAR(50),
@@ -9,7 +9,7 @@ CREATE TABLE Student (
     Phone VARCHAR(15),
     Department VARCHAR(30)
 );
-Course Table
+-- Course Table
 CREATE TABLE Course (
     Course_Code VARCHAR(10) PRIMARY KEY,
     Course_Name VARCHAR(50),
@@ -18,15 +18,15 @@ CREATE TABLE Course (
     Faculty_Email VARCHAR(50),
     FOREIGN KEY (Student_ID) REFERENCES Student(VTU_Number)
 );
-3. Insert Minimum 5 Records
-Insert Records into Student Table
+-- 3. Insert Minimum 5 Records
+-- Insert Records into Student Table
 INSERT INTO Student VALUES
 ('1VTU20CS001', 'Amit', 'amit@gmail.com', '9876543210', 'CSE'),
 ('1VTU20CS002', 'Bhavya', 'bhavya@gmail.com', '9876543211', 'CSE'),
 ('1VTU20EC003', 'Charan', 'charan@gmail.com', '9876543212', 'ECE'),
 ('1VTU20ME004', 'Divya', 'divya@gmail.com', '9876543213', 'ME'),
 ('1VTU20CS005', 'Esha', 'esha@gmail.com', '9876543214', 'CSE');
-Insert Records into Course Table
+-- Insert Records into Course Table
 INSERT INTO Course VALUES
 ('CS101', 'DBMS', 'F001', '1VTU20CS001', 'dbms@college.com'),
 ('CS102', 'OS', 'F002', '1VTU20CS002', 'os@college.com'),
